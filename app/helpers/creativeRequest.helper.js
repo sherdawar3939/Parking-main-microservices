3bcf9be67740b2e157485799c258f94d8a354c66
TAIMOOR HASSAN <taimoor@reactivespace.com>
2021-02-23 13:34:36 +0500

    byMergingWithSherMuhammad

--- app/helpers/creativeRequest.helper.js
+++ app/helpers/creativeRequest.helper.js
@@ -3,54 +3,54 @@
 var Sequelize = require('sequelize')
 const Op = Sequelize.Op
 const db = require('../config/sequelize.config')
-    // const _ = require('lodash')
+// const _ = require('lodash')
 
 /** Create Creative Requests */
-function createRequest(data) {
-    return db.CreativeRequest.findOne()
-        .then((result) => {
-            let foundUid = result.uid
+function createRequest (data) {
+  return db.CreativeRequest.findOne()
+    .then((result) => {
+      let foundUid = result.uid
 
-            data.uid = foundUid + 1
+      data.uid = foundUid + 1
 
-            return db.createRequest.create(data)
-        })
-}
-
-/** Fetch Creative Request List */
-function getRequestList(conditions, limit, offset) {
-    const where = {}
-
-    if (conditions.ClientId) {
-        where.ClientId = conditions.ClientId
-    }
-
-    if (conditions.status) {
-        where.status = conditions.status
-    }
-
-    if (conditions.search) {
-        where[[Op.or]] = {
-            qty: {
-                [Op.like]: '%' + conditions.search + '%'
-            }
-        }
-    }
-
-    return db.CreativeRequest.findAndCountAll({
-        raw: true,
-        nest: false,
-        where,
-        limit: limit,
-        offset: offset,
-        include: [{
-            model: db.Client,
-            as: 'creativeRequestClient'
-        }]
+      return db.createRequest.create(data)
     })
 }
 
+/** Fetch Creative Request List */
+function getRequestList (conditions, limit, offset) {
+  const where = {}
+
+  if (conditions.ClientId) {
+    where.ClientId = conditions.ClientId
+  }
+
+  if (conditions.status) {
+    where.status = conditions.status
+  }
+
+  if (conditions.search) {
+    where[[Op.or]] = {
+      qty: {
+        [Op.like]: '%' + conditions.search + '%'
+      }
+    }
+  }
+
+  return db.CreativeRequest.findAndCountAll({
+    raw: true,
+    nest: false,
+    where,
+    limit: limit,
+    offset: offset,
+    include: [{
+      model: db.Client,
+      as: 'creativeRequestClient'
+    }]
+  })
+}
+
 module.exports = {
-    createRequest,
-    getRequestList
+  createRequest,
+  getRequestList
 }
--- app/helpers/parking.helper.js
+++ app/helpers/parking.helper.js
@@ -3,50 +3,50 @@
 // var Sequelize = require('sequelize')
 // const Op = Sequelize.Op
 const db = require('../config/sequelize.config')
-    // const _ = require('lodash')
+// const _ = require('lodash')
 
 /** Create Creative Requests */
-function createParkingHelper(data) {
-    return db.Parking.create(data)
+function createParkingHelper (data) {
+  return db.Parking.create(data)
 }
 /** Fetch Creative Request List */
-function ActiveParkingListHelper(conditions, limit, offset) {
-    let parkingWhere = {}
-    let includes = [{
-        model: db.ParkingZone,
-        as: 'parkingZone',
-        attributes: ['zip', 'fee']
-    }]
+function ActiveParkingListHelper (conditions, limit, offset) {
+  let parkingWhere = {}
+  let includes = [{
+    model: db.ParkingZone,
+    as: 'parkingZone',
+    attributes: ['zip', 'fee']
+  }]
 
-    if (conditions.ParkingZoneId) {
-        parkingWhere.ParkingZoneId = conditions.ParkingZoneId
-    }
+  if (conditions.ParkingZoneId) {
+    parkingWhere.ParkingZoneId = conditions.ParkingZoneId
+  }
 
-    if (conditions.status) {
-        parkingWhere.status = conditions.status
-    }
+  if (conditions.status) {
+    parkingWhere.status = conditions.status
+  }
 
-    if (conditions.VehicleCategoryId) {
-        includes.push({
-            model: db.UserVehicle,
-            attributes: [],
-            where: {
-                VehicleCategoryId: conditions.VehicleCategoryId,
-                isDeleted: false
-            },
-            required: true
-        })
-    }
-
-    return db.Parking.findAll({
-        where: parkingWhere,
-        limit: limit,
-        offset: offset,
-        include: includes
+  if (conditions.VehicleCategoryId) {
+    includes.push({
+      model: db.UserVehicle,
+      attributes: [],
+      where: {
+        VehicleCategoryId: conditions.VehicleCategoryId,
+        isDeleted: false
+      },
+      required: true
     })
+  }
+
+  return db.Parking.findAll({
+    where: parkingWhere,
+    limit: limit,
+    offset: offset,
+    include: includes
+  })
 }
 
 module.exports = {
-    createParkingHelper,
-    ActiveParkingListHelper
+  createParkingHelper,
+  ActiveParkingListHelper
 }
