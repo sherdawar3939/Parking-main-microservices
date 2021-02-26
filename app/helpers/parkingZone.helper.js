99a01cec1fb80e573a4e5cdbb079432e42d7765e
Sher Muhammad <sherdawar210945@gmail.com>
2021-02-26 10:16:13 +0500

    commit after changes

--- app/helpers/parkingZone.helper.js
+++ app/helpers/parkingZone.helper.js
@@ -4,61 +4,61 @@ var Sequelize = require('sequelize')
 const Op = Sequelize.Op
 const db = require('../config/sequelize.config')
 
-function getparkingZone (conditions, limit, offset) {
-  const where = {}
+function getparkingZone(conditions, limit, offset) {
+    const where = {}
 
-  if (conditions.ClientId) {
-    where.ClientId = conditions.ClientId
-  }
-
-  if (conditions.CityId) {
-    where.CityId = conditions.CityId
-  }
-
-  if (conditions.search) {
-    where[[Op.or]] = {
-      days: {
-        [Op.like]: '%' + conditions.search + '%'
-      },
-      zip: {
-        [Op.like]: '%' + conditions.search + '%'
-      }
+    if (conditions.ClientId) {
+        where.ClientId = conditions.ClientId
     }
-  }
-  return db.ParkingZone.findAll({
-    where,
-    order: [
-      ['id', 'DESC']
-    ],
-    include: [{
-      model: db.Client,
-      as: 'parkingZoneClient'
-    }],
-    limit: limit,
-    offset: offset
-  })
+
+    if (conditions.CityId) {
+        where.CityId = conditions.CityId
+    }
+
+    if (conditions.search) {
+        where[Op.or] = {
+            days: {
+                [Op.like]: '%' + conditions.search + '%'
+            },
+            zip: {
+                [Op.like]: '%' + conditions.search + '%'
+            }
+        }
+    }
+    return db.ParkingZone.findAll({
+        where,
+        order: [
+            ['id', 'DESC']
+        ],
+        include: [{
+            model: db.Client,
+            as: 'parkingZoneClient'
+        }],
+        limit: limit,
+        offset: offset
+    })
 }
 const getParkingZoneId = (id) => {
-  return db.ParkingZone.findAll({
-    where: {
-      id
-    },
-    order: [
-      ['id', 'ASC']
-    ],
-    include: [{
-      attributes: ['id', 'companyName', 'email', 'address'],
-      model: db.Client,
-      as: 'parkingZoneClient'
-    }, {
-      model: db.CreativeRequest,
-      as: 'parkingCreatives'
-    }
+    return db.ParkingZone.findAll({
+        where: {
+            id
+        },
+        order: [
+            ['id', 'ASC']
+        ],
+        include: [{
+                attributes: ['id', 'companyName', 'email', 'address'],
+                model: db.Client,
+                as: 'parkingZoneClient'
+            }, {
+                model: db.CreativeRequest,
+                as: 'parkingCreatives'
+            }
 
-    ]
-  })
+        ]
+    })
 }
 module.exports = {
-  getparkingZone,
-  getParkingZoneId
+    getparkingZone,
+    getParkingZoneId
 }
