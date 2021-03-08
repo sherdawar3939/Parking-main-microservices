913488b4646216447ce01dd1fb6647118c252277
TAIMOOR HASSAN <taimoor@reactivespace.com>
2021-03-08 11:13:59 +0500

    dashboard ParkingCounts API done

--- app/middlewares/dashboard.middleware.js
+++ app/middlewares/dashboard.middleware.js
@@ -70,4 +70,24 @@ const validateGetClientsRevenue = (req, res, done) => {
   done()
 }
 
-module.exports = { validateGetClientsRevenue }
+const validateGetParkingCounts = (req, res, done) => {
+  const errorArray = []
+  const query = req.query
+  const validatedConditions = {}
+
+  if (query.hasOwnProperty('startDate') && query.startDate) {
+    validatedConditions.startDate = query.startDate
+  }
+  if (query.hasOwnProperty('endDate') && query.endDate) {
+    validatedConditions.endDate = query.endDate
+  }
+  if (!_.isEmpty(errorArray)) {
+    return generalMiddleware.standardErrorResponse(res, errorArray, 'dashboard.middleware.validateGetClientsRevenue')
+  }
+
+  req.conditions = validatedConditions
+
+  done()
+}
+
+module.exports = { validateGetClientsRevenue, validateGetParkingCounts }
