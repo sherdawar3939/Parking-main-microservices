19fe3da76fb41556ec53cf0c87173c08a5d60e17
Taimoor <engr.taimoor26@gmail.com>
2021-03-24 13:13:17 +0500

    pull/push HamzaAslam's Branch with parking Merge

---
+++ app/models/UserVoucher.model.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = function (sequelize, DataTypes) {
+  let UserVoucher = sequelize.define('UserVoucher', {
+    UserVehicleId: {
+      type: DataTypes.INTEGER(11)
+    },
+    UserId: {
+      type: DataTypes.INTEGER(11)
+    },
+    expiryDate: {
+      type: DataTypes.DATE
+    },
+    paymentStatus: {
+      type: DataTypes.ENUM(['Pending', 'Paid']),
+      allowNull: false,
+      defaultValue: 'Pending'
+    }
+  }, {
+    associate: function (models) {
+      UserVoucher.hasMany(models.Voucher, { foreignKey: 'UserVoucherId', as: 'UserVoucher' })
+    }
+  })
+  return UserVoucher
+}
