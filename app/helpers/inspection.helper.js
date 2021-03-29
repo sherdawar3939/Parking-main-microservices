1e82599d04e5f3d5f2853edf77a26df195020993
Sumair Rasool <sumairrasoolcs@gmail.com>
2021-03-29 16:20:40 +0500

    inspection api

--- app/helpers/inspection.helper.js
+++ app/helpers/inspection.helper.js
@@ -1,76 +1,71 @@
 'use strict'
 const db = require('../config/sequelize.config')
+const Op = db.Sequelize.Op
 var sequelize = require('sequelize')
 
 /** Fetch Parking Status */
-function ParkingStatusInspection(conditions) {
-    const where = {}
-    if (conditions.licensePlate) {
-        where.licensePlate = conditions.licensePlate
-    }
+function ParkingStatusInspection (conditions) {
+  const where = {}
+  if (conditions.licensePlate) {
+    where.licensePlate = conditions.licensePlate
+  }
 
-    return db.Inspection.findAll({
-        where,
-        include: [{
-            model: db.Parking,
-            as: 'parkingInspections',
-            attributes: ['status']
-                //   where: {}
-        }]
-    })
+  return db.Inspection.findAll({
+    where,
+    include: [{
+      model: db.Parking,
+      as: 'parkingInspections',
+      attributes: ['status']
+      //   where: {}
+    }]
+  })
 }
 
 // Fetch Inspection
-function getInspectionHelper(conditions, limit, offset) {
-    let Where = {}
-    const inspectionWhere = {}
+function getInspectionHelper (conditions, limit, offset) {
+  const includes = []
+  const inspectionWhere = {}
+  const inspectorWhere = {}
 
-    if (conditions.ClientId) {
-        inspectionWhere.ClientId = conditions.ClientId
-    }
+  if (conditions.ClientId) {
+    inspectorWhere.ClientId = conditions.ClientId
+  }
 
-    let includes = [{
-        model: db.Inspector,
-        as: 'Inspections',
-        where: inspectionWhere
-    }]
+  if (conditions.InspectorId) {
+    inspectionWhere.InspectorId = conditions.InspectorId
+  }
 
-    if (conditions.InspectorId) {
-        Where.InspectorId = conditions.InspectorId
-    }
+  if (conditions.ParkingZoneId) {
+    inspectionWhere.ParkingZoneId = conditions.ParkingZoneId
+  }
 
-    if (conditions.ParkingZoneId) {
-        Where.ParkingZoneId = conditions.ParkingZoneId
-    }
+  if (conditions.result) {
+    inspectionWhere.result = conditions.result
+  }
 
-    if (conditions.result) {
-        Where.result = conditions.result
-    }
-    if (conditions.fromDate) {
-        Where = [sequelize.Where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '>=', conditions.fromDate)]
-    }
-    if (conditions.toDate) {
-        Where = [sequelize.Where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '<=', conditions.toDate)]
-    }
-    if (conditions.fromDate && conditions.toDate) {
-        Where = {
-            [Op.and]: [
-                [sequelize.where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '>=', conditions.fromDate)],
-                [sequelize.where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '<=', conditions.toDate)]
-            ]
-        }
-    }
+  if (conditions.fromDate && conditions.toDate) {
+    inspectionWhere[Op.and] = [
+      [sequelize.where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '>=', conditions.fromDate)],
+      [sequelize.where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '<=', conditions.toDate)]
+    ]
+  } else if (conditions.fromDate) {
+    inspectionWhere[Op.and] = [sequelize.Where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '>=', conditions.fromDate)]
+  } else if (conditions.toDate) {
+    inspectionWhere[Op.and] = [sequelize.Where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '<=', conditions.toDate)]
+  }
 
-    console.log('include', includes)
-    return db.Inspection.findAndCountAll({
-        where: Where,
-        include: [{
-            model: db.Inspector,
-            as: 'Inspections',
-            where: inspectionWhere
-        }],
-        limit: limit,
-        offset: offset
-    })
+  includes.push({
+    model: db.Inspector,
+    as: 'Inspections',
+    where: inspectorWhere
+  })
+
+  return db.Inspection.findAndCountAll({
+    where: inspectionWhere,
+    include: includes,
+    limit: limit,
+    offset: offset
+  })
 }
+
 module.exports = { getInspectionHelper, ParkingStatusInspection }
