9fee5be60088809c8b73fb52c7805d18e2fa4600
Malik Taimoor <engr.taimoor26@gmail.com>
2021-03-30 12:29:10 +0500

    Commited Update Voucher

--- app/helpers/voucher.helper.js
+++ app/helpers/voucher.helper.js
@@ -130,18 +130,20 @@ function updateSeasonalPass (id, data, status) {
         })
       }
       foundVoucher = voucher
-
+      console.log(foundVoucher.dataValues)
       if (data.fee === foundVoucher.fee) {
         return voucher
       }
 
       return db.Client.findOne({ raw: true, where: { id: foundVoucher.ClientId } })
         .then(async client => {
+          console.log(client)
           if (client.type === 'Government') {
             data.uid = generalHelper.createUid(0, foundVoucher.zip, status)
           } else if (client.type === 'Private') {
             await db.ParkingZone.findOne({ raw: true, where: { zip: foundVoucher.zip, ClientId: foundVoucher.ClientId } })
               .then(parkingZone => {
+                console.log(parkingZone)
                 if (!parkingZone) {
                   return generalHelper.rejectPromise([{
                     field: 'UpdateVoucherHelper',
@@ -150,12 +152,13 @@ function updateSeasonalPass (id, data, status) {
                   }])
                 }
                 data.uid = generalHelper.createUid(parkingZone.clientCount, foundVoucher.zip, status)
+                console.log(data.uid)
               })
           }
           return db.Voucher.findOne({ where: { uid: data.uid } })
         })
         .then((foundVoucher) => {
-          if (foundVoucher) {
+          if (!foundVoucher) {
             return generalHelper.rejectPromise({
               field: 'id',
               error: 'voucher-0005',
@@ -168,12 +171,12 @@ function updateSeasonalPass (id, data, status) {
               type: 'Voucher',
               ClientId: foundVoucher.ClientId,
               RefId: foundVoucher.id
-            }
-          })
+            } })
         })
         .then((foundContract) => {
+          console.log(foundContract)
           const currentDate = new Date()
-          const contractData = JSON.parse(foundContract.dataValues.data)
+          const contractData = { updated: [], created: [] }
           contractData.updated.push(`${data.uid} ${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}\n`)
 
           generalHelper.generateVoucherContract(foundContract.dataValues.contractUrl, JSON.parse(JSON.stringify(contractData.created)), JSON.parse(JSON.stringify(contractData.updated)))
