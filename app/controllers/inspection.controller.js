8e8c1c332edee443514ff992f52070638c84e2e4
Sher Muhammad <sherdawar210945@gmail.com>
2021-03-31 13:43:14 +0500

    changes are commited

--- app/controllers/inspection.controller.js
+++ app/controllers/inspection.controller.js
@@ -5,24 +5,24 @@ const StandardError = require('standard-error')
 const generalController = require('./general.controller')
 
 // Fetch Inspection
-const getInspection = function (req, res) {
-  return getInspectionHelper(req.conditions)
-    .then(function (data) {
-      generalController.successResponse(res, 'Inspection fetched successfully.', data, 'inspection.controller.getInspectionHelper')
-    }).catch(StandardError, function (err) {
-      generalController.errorResponse(res, err, null, 'inspection.controller.getInspection', SERVER_RESPONSE.VALIDATION_ERROR)
-    }).catch(function (err) {
-      generalController.errorResponse(res, err, 'Please check originalError for details', 'inspection.controller.getInspectionHelper', SERVER_RESPONSE.INTERNAL_SERVER_ERROR)
-    })
+const getInspection = function(req, res) {
+    return getInspectionHelper(req.conditions, req.limit, req.offset)
+        .then(function(data) {
+            generalController.successResponse(res, 'Inspection fetched successfully.', data, 'inspection.controller.getInspectionHelper')
+        }).catch(StandardError, function(err) {
+            generalController.errorResponse(res, err, null, 'inspection.controller.getInspection', SERVER_RESPONSE.VALIDATION_ERROR)
+        }).catch(function(err) {
+            generalController.errorResponse(res, err, 'Please check originalError for details', 'inspection.controller.getInspectionHelper', SERVER_RESPONSE.INTERNAL_SERVER_ERROR)
+        })
 }
-const createInspection = function (req, res) {
-  return createInspectionHelper(req.validatedBody)
-    .then(function (data) {
-      generalController.successResponse(res, 'Inspection created successfully.', data, 'inspection.controller.createInspectionHelper')
-    }).catch(StandardError, function (err) {
-      generalController.errorResponse(res, err, null, 'inspection.controller.createInspectionHelper', SERVER_RESPONSE.VALIDATION_ERROR)
-    }).catch(function (err) {
-      generalController.errorResponse(res, err, 'Please check originalError for details', 'inspection.controller.createInspectionHelper', SERVER_RESPONSE.INTERNAL_SERVER_ERROR)
-    })
+const createInspection = function(req, res) {
+    return createInspectionHelper(req.validatedBody)
+        .then(function(data) {
+            generalController.successResponse(res, 'Inspection created successfully.', data, 'inspection.controller.createInspectionHelper')
+        }).catch(StandardError, function(err) {
+            generalController.errorResponse(res, err, null, 'inspection.controller.createInspectionHelper', SERVER_RESPONSE.VALIDATION_ERROR)
+        }).catch(function(err) {
+            generalController.errorResponse(res, err, 'Please check originalError for details', 'inspection.controller.createInspectionHelper', SERVER_RESPONSE.INTERNAL_SERVER_ERROR)
+        })
 }
 module.exports = { getInspection, createInspection }
--- app/helpers/inspection.helper.js
+++ app/helpers/inspection.helper.js
@@ -4,91 +4,92 @@ const Op = db.Sequelize.Op
 var sequelize = require('sequelize')
 
 // Fetch Inspection
-function getInspectionHelper (conditions, limit, offset) {
-  const includes = []
-  const inspectionWhere = {}
-  const inspectorWhere = {}
+function getInspectionHelper(conditions, limit, offset) {
+    const includes = []
+    const inspectionWhere = {}
+    const inspectorWhere = {}
 
-  if (conditions.ClientId) {
-    inspectorWhere.ClientId = conditions.ClientId
-  }
+    if (conditions.ClientId) {
+        inspectorWhere.ClientId = conditions.ClientId
+    }
 
-  if (conditions.InspectorId) {
-    inspectionWhere.InspectorId = conditions.InspectorId
-  }
+    if (conditions.InspectorId) {
+        inspectionWhere.InspectorId = conditions.InspectorId
+    }
 
-  if (conditions.ParkingZoneId) {
-    inspectionWhere.ParkingZoneId = conditions.ParkingZoneId
-  }
+    if (conditions.ParkingZoneId) {
+        inspectionWhere.ParkingZoneId = conditions.ParkingZoneId
+    }
 
-  if (conditions.licensePlate) {
-    inspectionWhere.licensePlate = conditions.licensePlate
-  }
+    if (conditions.licensePlate) {
+        inspectionWhere.licensePlate = conditions.licensePlate
+    }
 
-  if (conditions.result) {
-    inspectionWhere.result = conditions.result
-  }
+    if (conditions.result) {
+        inspectionWhere.result = conditions.result
+    }
 
-  if (conditions.fromDate && conditions.toDate) {
-    inspectionWhere[Op.and] = [
-      [sequelize.where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '>=', conditions.fromDate)],
-      [sequelize.where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '<=', conditions.toDate)]
-    ]
-  } else if (conditions.fromDate) {
-    inspectionWhere[Op.and] = [sequelize.Where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '>=', conditions.fromDate)]
-  } else if (conditions.toDate) {
-    inspectionWhere[Op.and] = [sequelize.Where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '<=', conditions.toDate)]
-  }
+    if (conditions.fromDate && conditions.toDate) {
+        inspectionWhere[Op.and] = [
+            [sequelize.where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '>=', conditions.fromDate)],
+            [sequelize.where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '<=', conditions.toDate)]
+        ]
+    } else if (conditions.fromDate) {
+        inspectionWhere[Op.and] = [sequelize.Where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '>=', conditions.fromDate)]
+    } else if (conditions.toDate) {
+        inspectionWhere[Op.and] = [sequelize.Where(sequelize.fn('date', sequelize.col('Inspection.createdAt')), '<=', conditions.toDate)]
+    }
+    console.log(inspectionWhere)
+    console.log(inspectorWhere)
 
-  console.log(inspectionWhere)
+    includes.push({
+        model: db.Inspector,
+        as: 'inspector',
+        where: inspectorWhere,
+        include: [{
+            model: db.User,
+            as: 'userInspector',
+            attributes: ['id', 'fName', 'lName', 'email']
+        }]
+    })
+    includes.push({
+        model: db.ParkingZone,
+        as: 'inspectedParkingZone',
+        attributes: ['id', 'uid'],
+        // required: false
+    })
 
-  includes.push({
-    model: db.Inspector,
-    as: 'inspector',
-    inspectorWhere,
-    include: [{
-      model: db.User,
-      as: 'userInspector',
-      attributes: ['id', 'fName', 'lName', 'email']
-    }]
-  })
-
-  includes.push({
-    model: db.ParkingZone,
-    as: 'inspectedParkingZone',
-    attributes: ['id', 'uid'],
-    required: false
-  })
-
-  return db.Inspection.findAndCountAll({
-    inspectionWhere,
-    include: includes,
-    limit: limit,
-    offset: offset
-  })
+    return db.Inspection.findAndCountAll({
+        where: inspectionWhere,
+        include: includes,
+        limit: limit,
+        offset: offset
+    })
 }
 
 // Create Inspection
-function createInspectionHelper (userData) {
-  return db.Parking.findOne({
-    where: { licensePlate: userData.licensePlate, status: 'Started' },
-    raw: true
-  })
-    .then((foundStatus) => {
-      const inspection = {
-        InspectorId: foundStatus.InspectorId,
-        licensePlate: foundStatus.licensePlate,
-        result: 'illegal'
-      }
+function createInspectionHelper(userData) {
+    return db.Parking.findOne({
+            where: { licensePlate: userData.licensePlate, status: 'Started' },
+            raw: true
+        })
+        .then((foundStatus) => {
+            // console.log(foundStatus);
+            console.log(userData)
+            const inspection = {
+                InspectorId: userData.InspectorId,
+                licensePlate: foundStatus.licensePlate,
+                result: 'illegal'
+            }
 
-      if (foundStatus) {
-        inspection.result = 'legal'
-        inspection.ParkingId = foundStatus.ParkingId
-        inspection.ParkingZoneId = foundStatus.ParkingZoneId
-      }
+            if (foundStatus) {
+                inspection.result = 'legal'
+                inspection.ParkingId = foundStatus.ParkingId
+                inspection.ParkingZoneId = foundStatus.ParkingZoneId
+            }
 
-      return db.Inspection.create(inspection)
-    })
+            return db.Inspection.create(inspection)
+        })
 }
 
 module.exports = { getInspectionHelper, createInspectionHelper }
