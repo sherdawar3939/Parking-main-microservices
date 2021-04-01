4804ab209a4af6f985250546f01299d42a2fe625
hamzaaslam76 <71928885+hamzaaslam76@users.noreply.github.com>
2021-04-01 10:28:54 +0500

    vehicle delete

--- app/helpers/vehicleCategory.helper.js
+++ app/helpers/vehicleCategory.helper.js
@@ -4,15 +4,16 @@ const db = require('../config/sequelize.config')
 // fetch VehicleCategory
 function getVehicleCategory (conditions) {
   return db.VehicleCategory.findAll({
-    where: conditions,
-    isDeleted: false
+
+    where: conditions
+    // isDeleted: false
   })
 }
 
 // Delete VehicleCategory
 function deleteVehicleCategory (id) {
   return db.VehicleCategory.update({ isDeleted: true }, {
-    where: { id }
+    where: { id: id }
   })
 }
 
